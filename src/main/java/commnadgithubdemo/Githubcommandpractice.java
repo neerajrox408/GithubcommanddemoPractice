474ff926a9b28d8265503ff8029f3421f767235a
neerajrox408 <neerajagrawal765@gmail.com>
2022-07-25 15:17:41 +0530

    done with github

---
+++ src/main/java/commnadgithubdemo/Githubcommandpractice.java
@@ -0,0 +1,14 @@
+package commnadgithubdemo;
+
+public class Githubcommandpractice {
+
+	public static void main(String[] args) {
+String browser="edge";
+if(browser=="edge")
+{
+	System.out.println("browser is true");
+}
+
+	}
+
+}
